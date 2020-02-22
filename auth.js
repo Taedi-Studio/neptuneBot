aeff05713a4576a0930d52be626822cf1aa950bb
PMH <pmhstudio.pmh@gmail.com>
2020-02-22 12:22:14 +0900

    Error Handling

--- auth.js
+++ auth.js
@@ -1,9 +1,9 @@
 const superagent = require('superagent')
 
 exports.discord = async (authData, code, oauthObj) => {
-  let data = await oauthObj.tokenRequest({ ...authData, code: code[0] })
-  let userData = await oauthObj.getUser(data.access_token)
-  
+  const data = await oauthObj.tokenRequest({ ...authData, code: code[0] })
+  const userData = await oauthObj.getUser(data.access_token)
+
   return {
     token: data.access_token,
     userData
@@ -11,12 +11,12 @@ exports.discord = async (authData, code, oauthObj) => {
 }
 
 exports.google = (token) => {
-  return new Promise((r, j) => {
+  return new Promise((resolve, reject) => {
     superagent.get('https://oauth2.googleapis.com/tokeninfo?id_token=' + token, (err, data) => {
-      if (err) j()
+      if (err) reject(err)
       else {
-        if (!data.body.email_verified) j()
-        else r(data)
+        if (!data.body.email_verified) reject(new Error('Email Not Verified'))
+        else resolve(data)
       }
     })
   })
