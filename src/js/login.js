1f730ea9b84c669ad5daff2142b6e7dde6ec51fa
PMH <pmhstudio.pmh@gmail.com>
2020-02-22 12:24:14 +0900

    Global Variables

--- src/js/login.js
+++ src/js/login.js
@@ -1,6 +1,8 @@
+/* global gapi */
 /* eslint-disable no-unused-vars */
+
 function onSignIn (googleUser) {
-  if(getParameterByName('key')) window.location.replace('/solve/google?code=' + [getParameterByName('key'), googleUser.getAuthResponse().id_token].join(';'))
+  if (getParameterByName('key')) window.location.replace('/solve/google?code=' + [getParameterByName('key'), googleUser.getAuthResponse().id_token].join(';'))
 }
 
 function getParameterByName (name, url) {
@@ -13,17 +15,17 @@ function getParameterByName (name, url) {
   return decodeURIComponent(results[2].replace(/\+/g, ' '))
 }
 
-function googleSignOut() {
+function googleSignOut () {
   gapi.auth2.getAuthInstance().signOut()
   window.location.replace('/login?key=' + getParameterByName('key').split(';')[0])
 }
 
-function onGoogleLoad() {
-  gapi.load('auth2', function() {
+function onGoogleLoad () {
+  gapi.load('auth2', () => {
     gapi.auth2.init()
   })
 }
 
-function submit() {
+function submit () {
   window.location.replace('/solve/submit?code=' + getParameterByName('key'))
 }
